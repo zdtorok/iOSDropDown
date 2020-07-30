68b4f010cd440c7dabdeec76420554bc8972d530
Zoltan Daniel Torok <dzsig.92@gmail.com>
2020-07-30 09:35:43 +0200

    Correct iOSDropDown changed

--- iOSDropDown/Classes/iOSDropDown.swift
+++ iOSDropDown/Classes/iOSDropDown.swift
@@ -10,6 +10,20 @@ import UIKit
 
 open class DropDown : UITextField{
 
+    let padding = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+
+    override open func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override open func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override open func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
     var arrow : Arrow!
     var table : UITableView!
     var shadow : UIView!
@@ -402,7 +416,7 @@ extension DropDown: UITableViewDataSource {
         cell?.textLabel?.font = self.font
         cell?.textLabel?.textAlignment = self.textAlignment
         cell?.textLabel?.numberOfLines = 0
-        cell?.textLabel?.lineBreakMode = .byWrodWrapping
+        cell?.textLabel?.lineBreakMode = .byWordWrapping
         return cell!
     }
 }
